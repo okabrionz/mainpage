0eae35eb428c72fd865b8c4ec9bf032a70c20ca9
Oka bRionZ <okabrionz@gmail.com>
2017-06-03 01:54:26 +0700

    holla

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'brionz',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Oka bRionZ';
+  desc = 'Jasa Pembuatan Situs Bootstrap 3 dan 4 untuk Company Profile atau Landing Page.'
+}
