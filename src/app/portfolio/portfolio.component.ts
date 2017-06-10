99ba7c1afc9b296bbecab7ca8e5f6b27d46d7a61
Oka bRionZ <okabrionz@gmail.com>
2017-06-10 22:34:24 +0700

    move modal title into portfolio ts

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./portfolio.component.css']
 })
 export class PortfolioComponent implements OnInit {
+  ptitle1 = 'Roti Rachmat';
+  ptitle2 = 'Managed Virtual Machine';
+  ptitle3 = 'Androholic';
+  ptitle4 = 'Web Host Indonesia';
+  ptitle5 = 'STID Sirnarasa';
+  ptitle6 = 'Pasantren Sirnarasa';
 
   constructor() { }
 
