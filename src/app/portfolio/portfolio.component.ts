c7db39e10796088b2be44f142f8a12485a8c7e04
Oka bRionZ <okabrionz@gmail.com>
2017-06-11 20:33:34 +0700

    moved img to ts

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -18,7 +18,22 @@ export class PortfolioComponent implements OnInit {
   pdesc3 = 'Blog tentang Android dan Teknologi terbaru.';
   pdesc4 = 'Layanan Web Hosting Indonesia';
   pdesc5 = 'Sekolah Tinggi Ilmu Dakwah Sirnarasa.';
-  pdesc6 = 'Pesantren Peradaban Dunia Sirnarasa.'
+  pdesc6 = 'Pesantren Peradaban Dunia Sirnarasa.';
+
+  psicon1 = 'assets/img/portfolio/rachmat-s.jpg';
+  psicon2 = 'assets/img/portfolio/managedvm-s.jpg';
+  psicon3 = 'assets/img/portfolio/circus.png';
+  psicon4 = 'assets/img/portfolio/game.png';
+  psicon5 = 'assets/img/portfolio/safe.png';
+  psicon6 = 'assets/img/portfolio/submarine.png';
+
+  pimg1 = 'assets/img/portfolio/rachmat.png';
+  pimg2 = 'assets/img/portfolio/managedvm-og.jpg';
+  pimg3 = 'assets/img/portfolio/circus.png';
+  pimg4 = 'assets/img/portfolio/game.png';
+  pimg5 = 'assets/img/portfolio/safe.png';
+  pimg6 = 'assets/img/portfolio/submarine.png';
+
 
   constructor() { }
 
