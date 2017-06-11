40e890920c3cacb98b1b088c71cf7662c56885ac
Oka bRionZ <okabrionz@gmail.com>
2017-06-12 04:03:51 +0700

    add androholic.com

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -22,14 +22,14 @@ export class PortfolioComponent implements OnInit {
 
   psicon1 = 'assets/img/portfolio/rachmat-s.jpg';
   psicon2 = 'assets/img/portfolio/managedvm-s.jpg';
-  psicon3 = 'assets/img/portfolio/circus.png';
+  psicon3 = 'assets/img/portfolio/androholic-s.png';
   psicon4 = 'assets/img/portfolio/game.png';
   psicon5 = 'assets/img/portfolio/safe.png';
   psicon6 = 'assets/img/portfolio/submarine.png';
 
   pimg1 = 'assets/img/portfolio/rachmat.png';
   pimg2 = 'assets/img/portfolio/managedvm-og.jpg';
-  pimg3 = 'assets/img/portfolio/circus.png';
+  pimg3 = 'assets/img/portfolio/androholic.jpg';
   pimg4 = 'assets/img/portfolio/game.png';
   pimg5 = 'assets/img/portfolio/safe.png';
   pimg6 = 'assets/img/portfolio/submarine.png';
