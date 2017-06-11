bc5b4ed76157c37b24ab5eba28ee038e4bf680a5
Oka bRionZ <okabrionz@gmail.com>
2017-06-12 04:16:09 +0700

    add default img

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -23,16 +23,16 @@ export class PortfolioComponent implements OnInit {
   psicon1 = 'assets/img/portfolio/rachmat-s.jpg';
   psicon2 = 'assets/img/portfolio/managedvm-s.jpg';
   psicon3 = 'assets/img/portfolio/androholic-s.png';
-  psicon4 = 'assets/img/portfolio/game.png';
-  psicon5 = 'assets/img/portfolio/safe.png';
-  psicon6 = 'assets/img/portfolio/submarine.png';
+  psicon4 = 'assets/img/portfolio/okabrionz-s.png';
+  psicon5 = 'assets/img/portfolio/okabrionz-s.png';
+  psicon6 = 'assets/img/portfolio/okabrionz-s.png';
 
   pimg1 = 'assets/img/portfolio/rachmat.png';
   pimg2 = 'assets/img/portfolio/managedvm-og.jpg';
   pimg3 = 'assets/img/portfolio/androholic.jpg';
-  pimg4 = 'assets/img/portfolio/game.png';
-  pimg5 = 'assets/img/portfolio/safe.png';
-  pimg6 = 'assets/img/portfolio/submarine.png';
+  pimg4 = 'assets/img/portfolio/okabrionz.jpg';
+  pimg5 = 'assets/img/portfolio/okabrionz.jpg';
+  pimg6 = 'assets/img/portfolio/okabrionz.jpg';
 
 
   constructor() { }
